0b5681c4e8ce121dc363d8a9920962906082120a
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-02-06 00:11:15 -0500

    Adjust access modifiers for lob and add constructors

--- crates/engine/src/lob/limitorderbook.rs
+++ crates/engine/src/lob/limitorderbook.rs
@@ -1,17 +1,38 @@
 use std::collections::BTreeMap;
 use std::collections::VecDeque;
 use crate::lob::types::{OrderId, Price};
+use crate::lob::order::Order;
 
 #[derive(Debug, Default)]
 pub struct PriceLevel {
-	pub total_qty: u64,
-	pub orders: VecDeque<OrderId>,
+	total_qty: u64,
+	orders: VecDeque<OrderId>,
+}
+impl PriceLevel {
+	pub fn new() -> Self {
+		Self {
+			total_qty: 0,
+			orders: VecDeque::new(),
+		}
+	}
 }
 
 #[derive(Debug, Default)]
 pub struct OrderBook {
 	best_buy: Price,
 	best_sell: Price,
+	orders: HashMap<OrderId, Order>,
 	buy_orders: BTreeMap<Price, PriceLevel>, 
 	sell_orders: BTreeMap<Price, PriceLevel>, 
+}
+impl OrderBook {
+	pub fn new() -> Self {
+		Self {
+			best_buy: 0,
+			best_sell: 0,
+			orders: HashMap::new(),
+			buy_orders: BTreeMap::new(),
+			sell_orders: BTreeMap::new(),
+		}
+	}
 }
