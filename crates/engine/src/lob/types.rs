bbfeac75adf673ea3586ac74d5ff8bf26a472bdc
Jacob Nickerson <150750217+JacobNickerson@users.noreply.github.com>
2026-02-14 06:59:08 -0500

    Implement Event Source (#5)
    * Implement LimitOrderBook module
    
    This commit adds the LimitOrderBook module, containing an OrderBook
    struct. The OrderBook supports insertion, deletion, and modification of
    an order. Price levels are stored in sorted order via a BTree and
    individual price levels are FIFO by time. On order insertion or
    modification, a match is naively made by a linear scan. Matches result
    in trades being made at the price of the pre-existing order. Deletions
    are handled lazily by marking orders as deleted, which are pruned when
    getting the best bid/ask or attempting to match.
    
    * Implement MarketOrder and refactor LimitOrder and LimitOrderBook
    
    This commit implements MarketOrder. In order to make this work,
    LimitOrder and LimitOrderBook were refactored as well. LimitOrder has
    now been separated into two structs: LimitOrder (which are stored in the
    LimitOrderBook) and Order (intended to be received from clients). Now,
    the order book has a function for handling all types of Orders.
    
    * Fix CI not testing engine crate
    
    Engine modules were not being imported into main.rs and as a result
    tests weren't being picked up by the test harness. Additionally, CI/CD
    rules have been slightly relaxed to allow warnings during compilation.
    
    * Implement EventSource and basic impls
    
    This adds EventSource, a trait that any data generator should implement
    for data generation. The first event source, PoissonSource, was
    implemented, which is composed of four components: a RateController, a
    TypeSelector, an OrderGenerator, and an EventRouter. RateController
    handles the calculation of inter-arrival times, TypeSelector handles the
    selection of order types and sides, OrderGenerator handles the
    calculation of price, qty, and id and generates the order, and
    EventRouter handles how the event is used.
    
    Several implementations were made for each of these traits:
    ConstantPoissonRate generates inter-arrival time deltas using an
    exponential distribution without changing over time. UniformTypeSelector
    uses an uniform distribution and configurable probabilities to select
    types, GaussianOrderGenerator selects price levels using a normal
    distribution with configurable parameters, and VectorStorageRouter
    receives orders and stores them in a vector.
    
    * Apply proper formatting
    
    Accidentally blew up the line count. Should've done it sooner but I'm
    new to Rust T_T
    
    ---------
    
    Co-authored-by: Jacob Nickerson <jacobmilesnickerson@gmail.com>
    Co-authored-by: Kathryn Jerez <65245095+KathyJDev@users.noreply.github.com>

--- crates/engine/src/lob/types.rs
+++ crates/engine/src/lob/types.rs
@@ -1,17 +1,3 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Side {
-	Buy,
-	Sell,
-	Bid,
-	Ask,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum OrderStatus {
-	Active,
-	Canceled,
-}
-
 pub type OrderId = u64;
 pub type Price = u64;
 pub type Timestamp = u64;
