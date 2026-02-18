eadb0e71fc0b9d87c24a08e8fde8b2d61857e06c
Kathryn Jerez <65245095+KathyJDev@users.noreply.github.com>
2026-02-18 13:12:29 -0500

    add strategies component and change dashboard to grid layout to fix sizing issues

--- gui/src/main.py
+++ gui/src/main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import ( 
-    QApplication, QWidget, QVBoxLayout, QHBoxLayout,
+    QApplication, QWidget, QGridLayout, QHBoxLayout,
     QSizePolicy
 )
 
@@ -9,7 +9,7 @@ from PyQt5.QtCore import (
 )
 from widgets.sidebar import SideBar
 from windows.dashboard import (
-    MarketEvents, OrderBook, TradeHistory, OrderEntry
+    MarketEvents, OrderBook, TradeHistory, OrderEntry, Strategies
 )
 
 class EngineWindow(QWidget):
@@ -31,36 +31,30 @@ class EngineWindow(QWidget):
         # Content Area
         content_widget = QWidget()
         content_widget.setStyleSheet("background-color: #080808;")
-        content_layout = QVBoxLayout(content_widget)
-        content_layout.setContentsMargins(0,0,0,0)
+        content_layout = QGridLayout(content_widget)
         content_layout.setContentsMargins(20, 64, 20, 20)
-        content_layout.setSpacing(20)
-        content_layout.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        content_layout.setHorizontalSpacing(20)
+        content_layout.setVerticalSpacing(20)
 
-        row_layout_1 = QHBoxLayout()
-        row_layout_1.setSpacing(20)
-        row_layout_2 = QHBoxLayout()
-        row_layout_2.setSpacing(20)
-
-        # MarketEvents
         self.market_events = MarketEvents()
-        row_layout_1.addWidget(self.market_events)
-
-        # OrderBook
         self.order_book = OrderBook()
-        row_layout_1.addWidget(self.order_book)
-
-        # OrderEntry
         self.order_entry = OrderEntry()
-        row_layout_1.addWidget(self.order_entry)
-
-        # TradeHistory
         self.trade_history = TradeHistory()
-        row_layout_2.addWidget(self.trade_history)
+        self.strategies = Strategies()
 
-        content_layout.addLayout(row_layout_1)
-        content_layout.addLayout(row_layout_2, 2)
-        content_layout.addStretch()
+        content_layout.addWidget(self.market_events, 0, 0)
+        content_layout.addWidget(self.order_book, 0, 1)
+        content_layout.addWidget(self.order_entry, 0, 2)
+
+        content_layout.addWidget(self.trade_history, 1, 0, 1, 2)
+        content_layout.addWidget(self.strategies, 1, 2)
+
+        content_layout.setColumnStretch(0, 6)  # MarketEvents
+        content_layout.setColumnStretch(1, 2)  # OrderBook
+        content_layout.setColumnStretch(2, 3)  # OrderEntry
+
+        content_layout.setRowStretch(0, 5)
+        content_layout.setRowStretch(1, 3)
 
         main_layout.addWidget(self.sidebar)
         main_layout.addWidget(content_widget)
