f78d16ef611eb243860c616b15b414654ee03bef
Jacob Nickerson <150750217+JacobNickerson@users.noreply.github.com>
2026-02-27 04:41:55 -0500

    Fix an issue where generated orders were incorrectly timestamped (#8)
    Generated orders were being timestamped with dt instead of with
    timestamps separated by dt. This was fixed, which also ensures that
    generated orders are monotonic in time.
    
    Co-authored-by: Jacob Nickerson <jacobmilesnickerson@gmail.com>

--- crates/engine/src/data_generator/event_source.rs
+++ crates/engine/src/data_generator/event_source.rs
@@ -52,3 +52,44 @@ impl<R: RateController, T: TypeSelector, G: OrderGenerator, E: EventRouter, N: R
         self.event_router.route_event(order);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::data_generator::{
+        event_router::VectorStorageRouter, order_generators::GaussianOrderGenerator,
+        rate_controllers::ConstantPoissonRate, type_selectors::UniformTypeSelector,
+    };
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    fn poisson_generator() -> PoissonSource<
+        ConstantPoissonRate,
+        UniformTypeSelector,
+        GaussianOrderGenerator,
+        VectorStorageRouter,
+        ChaCha8Rng,
+    > {
+        PoissonSource::new(
+            ConstantPoissonRate::new(1_000_000.0),
+            UniformTypeSelector::new(0.5, 0.4, 0.3, 0.2, 0.1),
+            GaussianOrderGenerator::new(15.0, 1.0),
+            VectorStorageRouter::new(),
+            ChaCha8Rng::seed_from_u64(0),
+        )
+    }
+    #[test]
+    fn orders_are_monotonic_in_time() {
+        let mut generator = poisson_generator();
+        for _ in 0..1_000_000 {
+            generator.next_event();
+        }
+        assert!(
+            generator
+                .event_router
+                .events
+                .windows(2)
+                .all(|e| e[0].timestamp <= e[1].timestamp)
+        );
+    }
+}
