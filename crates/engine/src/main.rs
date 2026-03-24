a8d8453a3b8e226c9fc7666efbfce1129245fdae
Jacob Nickerson <jacobnickerson817@gmail.com>
2026-03-24 01:05:57 -0400

    Add MoldEngine for event broadcasting

--- crates/engine/src/main.rs
+++ crates/engine/src/main.rs
@@ -1,25 +1,24 @@
-use mm_core::lob_core::market_events::{MarketEventType, SingleEventFeed};
-use mm_core::lob_core::{
-    market_orders::Order,
-    market_events::MarketEvent,
-};
+use mm_core::lob_core::market_events::{EventSink, MarketEventType, SingleEventFeed};
+use mm_core::lob_core::{market_events::MarketEvent, market_orders::Order};
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
 use ringbuf::{HeapRb, traits::*};
-use std::thread;
-use std::time::Instant;
 use std::fs::File;
 use std::io::{self, Write};
+use std::thread;
+use std::time::Instant;
 
 use crate::data_generator::event_source::PoissonSource;
 use crate::data_generator::order_generators::GaussianOrderGenerator;
 use crate::data_generator::rate_controllers::ConstantPoissonRate;
 use crate::data_generator::type_selectors::UniformTypeSelector;
+use crate::moldudp64::engine::MoldEngine;
 use crate::simulator::simulator::Simulator;
 
 mod data_generator;
 mod fix;
 mod lob;
+mod moldudp64;
 mod simulator;
 
 fn main() {
@@ -29,7 +28,7 @@ fn main() {
     let mut sim = Simulator::new(
         PoissonSource::new(
             ConstantPoissonRate::new(100_000.0),
-            UniformTypeSelector::new(0.5, 0.4,0.3,0.2,0.1),
+            UniformTypeSelector::new(0.5, 0.4, 0.3, 0.2, 0.1),
             GaussianOrderGenerator::new(1000.0, 500.0),
             ChaCha8Rng::seed_from_u64(0),
         ),
@@ -37,20 +36,11 @@ fn main() {
         user_order_cons,
         ChaCha8Rng::seed_from_u64(67),
     );
-    let mut trade_file = File::create("trades.txt").unwrap();
-    let mut order_file = File::create("orders.txt").unwrap();
-    let event_logger_thread = thread::spawn(move || {
+    let mut mold_engine = MoldEngine::start();
+    let event_broadcast_thread = thread::spawn(move || {
         loop {
             if let Some(order) = market_event_cons.try_pop() {
-                match order.kind {
-                    MarketEventType::L3(order) => {
-                        writeln!(order_file,"{:?}",order).unwrap();
-                    },
-                    MarketEventType::Trade(trade) => {
-                        writeln!(trade_file,"{:?}",trade).unwrap();
-                    },
-                    _ => {},
-                }
+                mold_engine.push(order);
             }
         }
     });
@@ -70,5 +60,5 @@ fn main() {
         elapsed.as_nanos() as f64 / 1_000_000_000.0,
         elapsed.as_nanos()
     );
-    let _ = event_logger_thread.join();
+    let _ = event_broadcast_thread.join();
 }
