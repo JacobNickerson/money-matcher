aa997022da29f407b69ec68909993779cf69b26b
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-03-28 13:31:05 -0400

    Change MoldEngine to run on localhost

--- crates/engine/src/moldudp64/engine.rs
+++ crates/engine/src/moldudp64/engine.rs
@@ -70,7 +70,7 @@ impl MoldEngine {
         socket.bind(&SockAddr::from(bind_addr)).expect("err");
         socket.set_multicast_ttl_v4(1).expect("err");
         socket
-            .set_multicast_if_v4(&Ipv4Addr::UNSPECIFIED)
+            .set_multicast_if_v4(&Ipv4Addr::LOCALHOST)
             .expect("err");
 
         let std_socket: UdpSocket = socket.into();
