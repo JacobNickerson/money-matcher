95922b2ca949f6553ed66fc0b18bc3189c240f8b
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-04-22 17:27:24 -0400

    Add Order serialization to a fixed-size byte structure

--- crates/mm_core/src/lob_core/market_orders.rs
+++ crates/mm_core/src/lob_core/market_orders.rs
@@ -53,6 +53,154 @@ impl Order {
             kind,
         }
     }
+
+    pub fn to_bytes(&self) -> [u8; size_of::<Order>()] {
+        let mut buf = [0u8; size_of::<Order>()];
+
+        const CLID_START: usize = 0;
+        const CLID_END: usize = size_of::<ClientId>();
+        buf[CLID_START..CLID_END].copy_from_slice(&self.client_id.to_le_bytes());
+
+        const ORDID_START: usize = CLID_END;
+        const ORDID_END: usize = ORDID_START + size_of::<OrderId>();
+        buf[ORDID_START..ORDID_END].copy_from_slice(&self.order_id.to_le_bytes());
+
+        const SIDE_START: usize = ORDID_END;
+        const SIDE_END: usize = SIDE_START + size_of::<OrderSide>();
+        buf[SIDE_START] = match self.side {
+            OrderSide::Ask => b'B',
+            OrderSide::Bid => b'S',
+        };
+        const TIME_START: usize = SIDE_END;
+        const TIME_END: usize = TIME_START + size_of::<Timestamp>();
+        buf[TIME_START..TIME_END].copy_from_slice(&self.timestamp.to_le_bytes());
+
+        const KIND_START: usize = TIME_END;
+        const KIND_END: usize = KIND_START + size_of::<OrderType>();
+        let mut kind = [0u8; size_of::<OrderType>()];
+        buf[KIND_START..KIND_END].copy_from_slice(match self.kind {
+            OrderType::Limit { qty, price } => {
+                kind[0] = 1;
+
+                const QTY_START: usize = 1;
+                const QTY_END: usize = QTY_START + size_of::<OrderQty>();
+                kind[QTY_START..QTY_END].copy_from_slice(&qty.to_le_bytes());
+
+                const PRICE_START: usize = QTY_END;
+                const PRICE_END: usize = PRICE_START + size_of::<Price>();
+                kind[PRICE_START..PRICE_END].copy_from_slice(&price.to_le_bytes());
+
+                &kind
+            }
+            OrderType::Market { qty } => {
+                kind[0] = 2;
+
+                const QTY_START: usize = 1;
+                const QTY_END: usize = QTY_START + size_of::<OrderQty>();
+                kind[QTY_START..QTY_END].copy_from_slice(&qty.to_le_bytes());
+
+                &kind
+            }
+            OrderType::Update { old_id, qty, price } => {
+                kind[0] = 3;
+
+                const OLDID_START: usize = 1;
+                const OLDID_END: usize = OLDID_START + size_of::<OrderId>();
+                kind[OLDID_START..OLDID_END].copy_from_slice(&old_id.to_le_bytes());
+
+                const QTY_START: usize = OLDID_END;
+                const QTY_END: usize = QTY_START + size_of::<OrderQty>();
+                kind[QTY_START..QTY_END].copy_from_slice(&qty.to_le_bytes());
+
+                const PRICE_START: usize = QTY_END;
+                const PRICE_END: usize = PRICE_START + size_of::<Price>();
+                kind[PRICE_START..PRICE_END].copy_from_slice(&price.to_le_bytes());
+
+                &kind
+            }
+            OrderType::Cancel { old_id } => {
+                kind[0] = 4;
+
+                const OLDID_START: usize = 1;
+                const OLDID_END: usize = OLDID_START + size_of::<OrderId>();
+                kind[OLDID_START..OLDID_END].copy_from_slice(&old_id.to_le_bytes());
+
+                &kind
+            }
+        });
+        buf
+    }
+
+    pub fn from_bytes(buf: [u8; size_of::<Order>()]) -> Self {
+        const CLID_START: usize = 0;
+        const CLID_END: usize = size_of::<ClientId>();
+        let client_id = ClientId::from_le_bytes(buf[CLID_START..CLID_END].try_into().unwrap());
+
+        const ORDID_START: usize = CLID_END;
+        const ORDID_END: usize = ORDID_START + size_of::<OrderId>();
+        let order_id = OrderId::from_le_bytes(buf[ORDID_START..ORDID_END].try_into().unwrap());
+
+        const SIDE_START: usize = ORDID_END;
+        const SIDE_END: usize = SIDE_START + size_of::<OrderSide>();
+        let side = match buf[SIDE_START] {
+            b'B' => OrderSide::Ask,
+            b'S' => OrderSide::Bid,
+            _ => panic!("error: attempted to deserialize an unknown side tag"),
+        };
+
+        const TIME_START: usize = SIDE_END;
+        const TIME_END: usize = TIME_START + size_of::<Timestamp>();
+        let timestamp = OrderId::from_le_bytes(buf[TIME_START..TIME_END].try_into().unwrap());
+
+        const KIND_START: usize = TIME_END;
+        let kind = match buf[KIND_START] {
+            1 => {
+                const QTY_START: usize = KIND_START + 1;
+                const QTY_END: usize = QTY_START + size_of::<OrderQty>();
+                const PRICE_START: usize = QTY_END;
+                const PRICE_END: usize = PRICE_START + size_of::<Price>();
+                OrderType::Limit {
+                    qty: OrderQty::from_le_bytes(buf[QTY_START..QTY_END].try_into().unwrap()),
+                    price: Price::from_le_bytes(buf[PRICE_START..PRICE_END].try_into().unwrap()),
+                }
+            }
+            2 => {
+                const QTY_START: usize = KIND_START + 1;
+                const QTY_END: usize = QTY_START + size_of::<OrderQty>();
+                OrderType::Market {
+                    qty: OrderQty::from_le_bytes(buf[QTY_START..QTY_END].try_into().unwrap()),
+                }
+            }
+            3 => {
+                const OLDID_START: usize = KIND_START + 1;
+                const OLDID_END: usize = OLDID_START + size_of::<OrderId>();
+                const QTY_START: usize = OLDID_END;
+                const QTY_END: usize = QTY_START + size_of::<OrderQty>();
+                const PRICE_START: usize = QTY_END;
+                const PRICE_END: usize = PRICE_START + size_of::<Price>();
+                OrderType::Update {
+                    old_id: OrderId::from_le_bytes(buf[OLDID_START..OLDID_END].try_into().unwrap()),
+                    qty: OrderQty::from_le_bytes(buf[QTY_START..QTY_END].try_into().unwrap()),
+                    price: Price::from_le_bytes(buf[PRICE_START..PRICE_END].try_into().unwrap()),
+                }
+            }
+            4 => {
+                const OLDID_START: usize = KIND_START + 1;
+                const OLDID_END: usize = OLDID_START + size_of::<OrderId>();
+                OrderType::Cancel {
+                    old_id: OrderId::from_le_bytes(buf[OLDID_START..OLDID_END].try_into().unwrap()),
+                }
+            }
+            _ => panic!("error: attempted to deserialize an unknown OrderType"),
+        };
+        Self {
+            client_id,
+            order_id,
+            side,
+            timestamp,
+            kind,
+        }
+    }
 }
 
 impl PartialOrd for Order {
@@ -148,5 +296,80 @@ impl LimitOrder {
                 panic!("LimitOrder cannot be constructed from an Order representing a cancel");
             }
         }
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_limit_serialization() {
+        let order = Order::new(
+            61268,
+            5819515,
+            OrderSide::Ask,
+            352895656,
+            OrderType::Limit {
+                qty: 357826,
+                price: 9659,
+            },
+        );
+
+        let bytes = order.to_bytes();
+        let deserialized = Order::from_bytes(bytes);
+
+        assert_eq!(order, deserialized);
+    }
+
+    #[test]
+    fn test_market_serialization() {
+        let order = Order::new(
+            61268,
+            5819515,
+            OrderSide::Bid,
+            352895656,
+            OrderType::Market { qty: 357826 },
+        );
+
+        let bytes = order.to_bytes();
+        let deserialized = Order::from_bytes(bytes);
+
+        assert_eq!(order, deserialized);
+    }
+
+    #[test]
+    fn test_update_serialization() {
+        let order = Order::new(
+            61268,
+            5819515,
+            OrderSide::Bid,
+            352895656,
+            OrderType::Update {
+                old_id: 3855,
+                qty: 0,
+                price: Price::MAX,
+            },
+        );
+
+        let bytes = order.to_bytes();
+        let deserialized = Order::from_bytes(bytes);
+
+        assert_eq!(order, deserialized);
+    }
+
+    #[test]
+    fn test_cancel_serialization() {
+        let order = Order::new(
+            61268,
+            5819515,
+            OrderSide::Ask,
+            352895656,
+            OrderType::Cancel { old_id: 99 },
+        );
+
+        let bytes = order.to_bytes();
+        let deserialized = Order::from_bytes(bytes);
+
+        assert_eq!(order, deserialized);
     }
 }
